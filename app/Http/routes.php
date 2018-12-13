c2a66db943a25c89e7a77577ba72ec32ae59c22d
sa <spiritbro59@yandex.com>
2018-12-13 04:45:32 +0000

    bismillah

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,15 +15,88 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/search/cities/', function () {
+    try{
+    $key=$_GET['key'];
     $id=$_GET['id'];
-    $flights = App\City::where('city_id', $id)->get();
     
-    return response()->json($flights);
+    if(empty($key)&&empty($id)){
+    return response()->json(["error"=>"key yang anda masukkan salah silahkan login terlebih dahulu"]);        
+    }else{
+        $flights = App\Orang::where('key', $key)->get();
+        if(count($flights)==0){
+    return response()->json(["error"=>"key yang anda masukkan salah silahkan login terlebih dahulu"]);                
+        }else{
+    $flights = App\City::where('city_id', $id)->get();
+    return response()->json($flights);    
+        }
+    }
+    
+    
+    
+    }catch(Exception $e){
+    return response()->json(["error"=>"key yang anda masukkan salah silahkan login terlebih dahulu"]);            
+    }
+    
 });
 Route::get('/search/provinces/', function () {
-  $id=$_GET['id'];
-  $flights = App\Province::where('province_id', $id)->get();
+    try{
+    $key=$_GET['key'];
+    $id=$_GET['id'];
     
-    return response()->json($flights);
+    if(empty($key)&&empty($id)){
+    return response()->json(["error"=>"key yang anda masukkan salah silahkan login terlebih dahulu"]);        
+    }else{
+        $flights = App\Orang::where('key', $key)->get();
+        if(count($flights)==0){
+    return response()->json(["error"=>"key yang anda masukkan salah silahkan login terlebih dahulu"]);                
+        }else{
+    $flights = App\Province::where('city_id', $id)->get();
+    return response()->json($flights);    
+        }
+    }
+    
+    
+    
+    }catch(Exception $e){
+    return response()->json(["error"=>"key yang anda masukkan salah silahkan login terlebih dahulu"]);            
+    }
 });
-
+Route::get('/login', function () {
+    try{
+  $user=$_GET['user'];
+  $pass=$_GET['pass'];
+  $flights = App\Orang::where('user', $user)->where('pass', $pass)->get();      
+  if(count($flights)==0){
+      return response()->json(["error"=>"username atau password salah"]);    
+  }else{
+  $cubi=hash('sha256', $user.$pass);
+  $flights = App\Orang::where('user', $user)->where('pass', $pass);
+  $flights->update(['key' => $cubi]);
+  
+  return response()->json(["key"=>$cubi,"keterangan"=>"gunakan key untuk search/cities?id={city_id} atau search/provinces?id={province_id}"]);    
+  }
+  
+    }catch(Exception $e){
+    return response()->json(["error"=>$e]);    
+    }
+  
+    
+    
+});
+Route::get('/register', function () {
+    try{
+  $user=$_GET['user'];
+  $pass=$_GET['pass'];      
+  if(!empty($user)&&!empty($pass)){
+  $user = App\Orang::firstOrCreate(['user' => $user,'pass' => $pass]);
+    return response()->json($user);    
+  }else{
+      return response()->json(["error"=>"mohon isi username pass"]);      
+  }
+    }catch(Exception $e){
+      return response()->json(["error"=>"mohon isi username pass"]);      
+    }
+  
+  
+  
+});
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ApiTest extends TestCase
 {
     /**
-     * A basic functional test example.
+     * A basic test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        
+        $this->assertTrue(true);
     }
 }
